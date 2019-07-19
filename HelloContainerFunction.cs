a23635c3fe9f9c64b639c90995d04eb7136773f5
Pankaj Rawat <pankaj.ra@hcl.com>
2019-07-19 13:11:53 +0530

    Initial Code

---
+++ HelloContainerFunction.cs
@@ -0,0 +1,25 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace ContainerizedFunctionApp.Function
+{
+    public static class HelloContainerFunction
+    {
+        [FunctionName("HelloContainerFunction")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            return (ActionResult)new OkObjectResult($"Hello, I'm running on docker container!");
+        }
+    }
+}
